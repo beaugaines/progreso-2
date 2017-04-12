8e9506de47a23076d0ecdc8cd1be8cfd702d7b95
Jose Mena <josemena0114@gmail.com>
2017-04-12 10:18:37 +0000

    projects and updates controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/edit'
+
+  get 'updates/index'
+
+  get 'updates/show'
+
+  get 'updates/new'
+
+  get 'updates/edit'
+
   get 'welcome/index'
   
   devise_for :users
