b89de1d6730be86d353555e8717775264f660545
Charlie Gaines <beaugaines@gmail.com>
2017-04-21 20:16:14 +0100

    sync text notifications working

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -9,6 +9,14 @@ class UpdatesController < ApplicationController
     @project = Project.find(params[:project_id])
     @update = @project.updates.new(update_params)
     if @update.save
+      @twilio_number = ENV['TWILIO_NUMBER']
+      notice = "We got an update heah on #{@project.title}! #{url_for([@project, @update])}"
+      # binding.pry
+      Twilio::REST::Client.new.messages.create(
+        from: @twilio_number,
+        to: '+12038227132',
+        body: notice
+      )
       flash[:notice] = 'Update added'
     else
       flash[:alert] = 'Update failed to save'
